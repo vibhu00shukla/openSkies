d24065dea07203162b0901c639ed6d60952f36b7
Vibhu <vibhu00shukla@gmail.com>
2025-06-14 05:23:45 +0530

    `Added currentUser variable to res.locals and conditional edit/delete buttons to campground show page`

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.user = req.user;
+  res.locals.currentUser = req.user;
   res.locals.success =req.flash('success');
   res.locals.error = req.flash('error');
   next();});
