19b4fe0a5c0b959d070535a320c55fc94b90f612
Vibhu <vibhu00shukla@gmail.com>
2025-06-16 00:52:52 +0530

    set the .env

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync'); 
 const Campground = require('../models/campground');
 const campgroundController = require('../controllers/campgroundController');
+const multer = require('multer');// Middleware for handling multipart/form-data, which is used for uploading files
+const upload =multer({ dest: 'uploads/' }); // Configure multer for file uploads
 
 const Joi = require('joi');
 const {isLoggedIn,isAuthor,validateCampground} = require('../middleware');
@@ -10,7 +12,11 @@ const { renderNewForm } = require('../controllers/campgroundController');
 
 router.route('/')
   .get(catchAsync(campgroundController.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground));
+  // .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground));
+  .post(upload.array('image'),(req,res) => {
+    console.log(req.body,req.files)
+    res.send('File uploaded successfully');
+  })
 
 router.get('/new', isLoggedIn, renderNewForm);
 
